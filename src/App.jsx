d34882d7b9f5189a4e8d55c78a43dc354ba4fc05
MehmetAydar01 <mehmet.aydar07@gmail.com>
2023-12-24 00:41:04 +0300

    Components added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from './Hero';
+import Projects from './Projects';
+
+const App = () => {
+  return (
+    <main>
+      <Hero />
+      <Projects />
+    </main>
+  );
+};
+
+export default App;
